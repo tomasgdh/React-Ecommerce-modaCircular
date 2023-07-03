891f0629f9f831534d8baad507eb574bb7782685
tomasgdh <tomasgdh@gmail.com>
2023-07-02 22:37:44 -0300

    varios

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRvYt477_Rq8moDogULfcuamHxygLLtno",
+  authDomain: "fb-ecommercemodacircular.firebaseapp.com",
+  projectId: "fb-ecommercemodacircular",
+  storageBucket: "fb-ecommercemodacircular.appspot.com",
+  messagingSenderId: "536555880580",
+  appId: "1:536555880580:web:149d0926113013d86c82d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
